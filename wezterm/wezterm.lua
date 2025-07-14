7e2614742d63e23dd8e5c3fd8b663b256ccf4f81
Steven Gardiner <gardiner@fnal.gov>
2025-07-13 21:38:19 -0500

    Add key mappings for copy/paste to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,17 +16,41 @@ config.tab_bar_at_bottom = true
 -- Allow resizing but hide the title bar
 config.window_decorations = "RESIZE"
 
+-- Define some custom key mappings to handle copy/paste in a reasonable
+-- way. This configuration is based on
+-- https://github.com/wezterm/wezterm/discussions/2426#discussioncomment-4197449
+config.keys = {
+  {
+    key = 'c',
+    mods = 'CTRL',
+    action = wezterm.action_callback(function(window, pane)
+      local sel = window:get_selection_text_for_pane(pane)
+      if (not sel or sel == '') then
+        window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane)
+      else
+        window:perform_action(wezterm.action{ CopyTo = 'ClipboardAndPrimarySelection' }, pane)
+      end
+    end),
+  },
+  { key = 'v', mods = 'CTRL', action = wezterm.action.PasteFrom 'Clipboard' },
+  { key = 'v', mods = 'SHIFT|CTRL', action = wezterm.action_callback(function(window, pane)
+    window:perform_action(wezterm.action.SendKey{ key='v', mods='CTRL' }, pane) end),
+  },
+  { key = 'V', mods = 'SHIFT|CTRL', action = wezterm.action_callback(function(window, pane)
+    window:perform_action(wezterm.action.SendKey{ key='v', mods='CTRL' }, pane) end),
+  },
+  { key = 'c', mods = 'ALT', action = wezterm.action{ CopyTo = 'ClipboardAndPrimarySelection' } },
+  { key = 'v', mods = 'ALT', action = wezterm.action.PasteFrom 'Clipboard' },
+}
+
 -- Use ALT + number to activate the corresponding tab
-local mykeys = {}
 for i = 1, 8 do
-table.insert(mykeys, {
+table.insert(config.keys, {
 key=tostring(i),
 mods="ALT",
 action=wezterm.action{ActivateTab=i-1},
 })
 end
-
-config.keys = mykeys
 
 -- Set the color scheme
 --config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
