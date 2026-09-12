ef127a215334bb9ff72aafeb25ff47fc79abb574
Steven Gardiner <gardiner@fnal.gov>
2026-09-12 18:53:36 -0500

    Change default terminal size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,6 +8,10 @@ require("resize_title")
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- Set default window size
+config.initial_cols = 82
+config.initial_rows = 27
+
 -- Tab configuration
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
